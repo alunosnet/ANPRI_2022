02aa83b9ff0690bdf2ed7e6bfccb006ffe4e3f25
alunosNET <alunosnet@gmail.com>
2022-02-26 18:57:40 +0000

    Pequenas correções

--- Sessao13/PlayerInteract.cs
+++ Sessao13/PlayerInteract.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,17 @@ public class PlayerInteract : MonoBehaviour
 {
     [SerializeField] float Distancia = 3;
     [SerializeField] LayerMask Layers;
-    Camera camera;
+    Camera _camera;
+    CinemachineVirtualCamera _cinemachine;
+    CinemachineTransposer _transposer;
+    float _distanciaPlayer = 0;
     // Start is called before the first frame update
     void Start()
     {
-        camera = FindObjectOfType<Camera>();
+        _camera = FindObjectOfType<Camera>();
+        _cinemachine = FindObjectOfType<CinemachineVirtualCamera>();
+        _transposer = _cinemachine.GetCinemachineComponent<CinemachineTransposer>();
+        _distanciaPlayer = Mathf.Abs(_transposer.m_FollowOffset.z);
     }
 
     // Update is called once per frame
@@ -18,9 +25,9 @@ public class PlayerInteract : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire2"))
         {
-            Vector3 origem =camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.5f)); //transform.position;
+            Vector3 origem =_camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.5f)); //transform.position;
             RaycastHit hit;
-            if(Physics.Raycast(origem, camera.transform.forward,out hit, Distancia, Layers))
+            if(Physics.Raycast(origem, _camera.transform.forward,out hit, _distanciaPlayer+Distancia, Layers))
             {
                 //Debug.Log("Interact");
                 var objeto = hit.collider.GetComponent<IInteract>();
